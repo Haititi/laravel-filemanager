4d70924c3db9a8fdd04f31a73a2141dffa8cd872
Taha Paksu <taha.paksu@edata.com.tr>
2019-11-01 15:14:42 +0300

    fixed url prefix at demo page

--- src/views/demo.blade.php
+++ src/views/demo.blade.php
@@ -61,7 +61,7 @@
   <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.3/umd/popper.min.js"></script>
   <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/js/bootstrap.min.js"></script>
   <script>
-   var route_prefix = "{{ url(config('lfm.url_prefix')) }}";
+    var route_prefix = "{{ URL::to(config('lfm.url_prefix', 'laravel-filemanager')) }}";
   </script>
 
   <!-- CKEditor init -->
